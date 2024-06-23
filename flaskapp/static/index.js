406be4cf44b187b11bfa1534c65a4633f5cdc8f5
Rodrigo Caña <alejandro.cana.v@gmail.com>
2024-06-23 12:48:43 -0400

    Style: improve chat style and add features that timestamp and user Prf
    User profiles are only first letter of the nicknae for now
    
    Issues:
    - chat window not displays overflow menu when messages overflowing,
      "vanishing" this messages.

--- flaskapp/static/index.js
+++ flaskapp/static/index.js
@@ -3,30 +3,53 @@ const chatButton = document.querySelector(".chat-controls button")
 const chatForm = document.querySelector(".chat-form")
 const chatWindow = document.querySelector(".chat-window")
 
+console.log(chatInput)
+console.log(chatButton)
+console.log(chatForm)
+console.log(chatWindow)
+
 let socket = io()
 let thisIsMyMessage = false
 
 const sendMessage = (e) => {
     e.preventDefault()
     
-    message = chatInput.value
+    const message = chatInput.value
     chatInput.value = ""
 
     thisIsMyMessage = true
-    console.log(message)
-    if (message) {
+    if (message.replace(/\s/g, '') != "") {
         socket.emit('chat-message', message)
     }
 }
-
+/*
+                <div class="--left">
+                    <div class="chat-message --userA">
+                        <div class="chat-message__time-user --t-left"><p>10:00AM</p><div><p>u</p></div></div>
+                        <div class="chat-message__inner">
+                            <p>message we Lorem, ipsum dolor sit amet consectetur adipisicing elit. In non voluptatem ratione voluptatum neque tenetur minus quaerat necessitatibus enim optio temporibus aspernatur totam officia inventore, dignissimos illum! Architecto asperiores quod consequuntur sed! Cum suscipit, saepe ducimus quas deserunt rerum voluptates fugiat sunt doloremque, libero, magni ea ratione impedit. Pariatur, rem.</p>
+                        </div>
+                    </div>
+                </div>
+*/
 
 socket.on('broadcast-message', (msg) => {
 
-  console.log(msg)
 
-  messageComponent = document.createElement('DIV')
+  console.log(msg)
+  const messageComponent = document.createElement('DIV')
   messageComponent.className = thisIsMyMessage ? "--right" : "--left"
-  messageComponent.innerHTML = `<div class="chat-message --userA"><p>${msg}</p></div>`
+
+  messageComponent.innerHTML = `
+  <div class="chat-message --userA">
+      <div class="chat-message__time-user ${thisIsMyMessage ? "--t-right" : "--t-left"}">
+        <p>10:00AM</p><div><p>u</p></div>
+      </div>
+      <div class="chat-message__inner">
+          <p>${msg}</p>
+      </div>
+  </div>
+  `
 
   chatWindow.appendChild(messageComponent)
   thisIsMyMessage = false
